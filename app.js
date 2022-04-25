67cd6210437afb351d4bdc7d5f4d3d0db63444e9
idaism <ida.bjarney@outlook.com>
2022-04-25 13:56:23 +0100

    nav transform and minor edits to midpage

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const menuIcon = document.querySelector('#navButton i');
 
 //SCROLL const----
 const headerText = document.getElementById('headerText');
+const header = document.getElementById('header');
 
 //DESTINATIONS const----
 const destinationsList = document.getElementById('destinationsList');
@@ -24,28 +25,34 @@ destinationsListButton.addEventListener('click', toggleDestinationsOpen);
 
 //WRAPPER event ----
 wrapper.addEventListener('click', function(){
-  if(!nav.classList.contains('closed')){
+  if(document.body.classList.contains('navOpen')){
     toggleClosed()
   }
 })
 
 //NAV function----
+// function toggleClosed() {
+//   nav.classList.toggle('closed');
+//   menuIcon.classList.toggle('fi-xtluxl-three-bars');
+//   menuIcon.classList.toggle('fi-ctluxl-times');
+//   destinationsList.classList.remove('open')
+// };
+
 function toggleClosed() {
-  nav.classList.toggle('closed');
-  menuIcon.classList.toggle('fi-xtluxl-three-bars');
-  menuIcon.classList.toggle('fi-ctluxl-times');
+  document.body.classList.toggle('navOpen');
   destinationsList.classList.remove('open')
 };
 
 //SCROLL function----
 function onScroll(event) {
   const scrollPosition = window.scrollY;
+  if(scrollPosition > header.getBoundingClientRect().height) return;
   headerText.style.transform = `translateY(${scrollPosition/1.4}px) scale(${1-scrollPosition/1200})`;
 };
 
 //DESTINATIONS function ----
 function toggleDestinationsOpen() {
-  if(nav.classList.contains('closed')) return;
+  if(!document.body.classList.contains('navOpen')) return;
   
   
   destinationsList.classList.toggle('open');
